52e4c82dc589929616a91595ff80534d0d267be4
Aaron Sananes <aasananes@gmail.com>
2019-06-21 10:27:37 +0100

    Added toggle pricing

--- web/src/components/Switch/Switch.js
+++ web/src/components/Switch/Switch.js
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "./Switch.module.scss";
 
-const Switch = ({}) => (
-  <label className={`${styles["label"]} ${styles["toggle"]}`}>
-    <input type="checkbox" className={styles["toggle_input"]} />
-    <div className={styles["toggle-control"]} />
-  </label>
-);
+const Switch = props => {
+  const { toggle } = props;
+  return (
+    <label className={`${styles["label"]} ${styles["toggle"]}`}>
+      <input
+        onChange={props.toggle}
+        type="checkbox"
+        className={styles["toggle_input"]}
+      />
+      <div className={styles["toggle-control"]} />
+    </label>
+  );
+};
 
 export default Switch;
--- web/src/pages/PricingPage/PricingPage.js
+++ web/src/pages/PricingPage/PricingPage.js
@@ -1,13 +1,12 @@
 // @flow
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import Icon from "../../components/Icon";
 import Layout from "../../components/Layout";
 import Button from "../../components/Button";
 import Page from "../../components/Page";
 import Switch from "../../components/Switch";
 import styles from "./PricingPage.module.scss";
-import { usePricingList, useSiteMetadata } from "../../hooks";
 
 // SVGs
 import enterpriseSVG from "../../assets/svg/pricing/enterprise.svg";
@@ -19,194 +18,224 @@ import smallBusinessHoverSVG from "../../assets/svg/pricing/small-business-hover
 import companySVG from "../../assets/svg/pricing/company.svg";
 import companyHoverSVG from "../../assets/svg/pricing/company-hover.svg";
 
-const PricingTemplate = ({ data }) => {
-  const { title, subtitle } = useSiteMetadata();
-  const pricingList = usePricingList();
+export const query = graphql`
+  {
+    sanityPricing {
+      _rawPackage(resolveReferences: { maxDepth: 6 })
+    }
+  }
+`;
 
-  return (
-    <Layout title={`Pricing - ${title}`} description={subtitle}>
-      <Page
-        title="Pricing that suits your needs"
-        subtitle="
+class PricingTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showPrice: false };
+  }
+
+  togglePrice = () => {
+    this.setState({
+      showPrice: !this.state.showPrice
+    });
+  };
+
+  componentDidUpdate() {}
+
+  componentDidMount() {
+    this.setState({ showPrice: false });
+  }
+
+  render() {
+    const { data } = this.props;
+    const pricingList = data.sanityPricing._rawPackage;
+    const annualPrice = this.state.showPrice;
+
+    return (
+      <Layout title={`Pricing`}>
+        <Page
+          title="Pricing that suits your needs"
+          subtitle="
         Conflux is perfect for individuals and teams of all sizes. Start for free."
-        center={true}
-      >
-        <div className={styles["pricing__toggle"]}>
-          <span
-            className={`${styles["pricing__toggle-label"]} ${
-              styles["pricing__toggle-label--month"]
-            }`}
-          >
-            Monthly
-          </span>
-          <Switch />
-          <span
-            className={`${styles["pricing__toggle-label"]} ${
-              styles["pricing__toggle-label--year"]
-            }`}
-          >
-            Anually
-          </span>
-        </div>
-
-        {/* <Signup className={styles["pricing__signup"]} /> */}
-        <div className={styles["pricing__wrapper"]}>
-          <div className={styles["pricing__pattern"]} />
-          <ul className={styles["pricing__list"]}>
-            {pricingList &&
-              pricingList.map(price => (
-                <li key={price._key} className={styles["pricing__item"]}>
-                  {price.name === "Hobby" ? (
-                    <img
-                      className={styles["pricing__item-image"]}
-                      src={hobbyHoverSVG}
-                    />
-                  ) : (
-                    ""
-                  )}
-                  {price.name === "Small Business" ? (
-                    <img
-                      className={styles["pricing__item-image"]}
-                      src={smallBusinessHoverSVG}
-                    />
-                  ) : (
-                    ""
-                  )}
-                  {price.name === "Company" ? (
-                    <img
-                      className={styles["pricing__item-image"]}
-                      src={companyHoverSVG}
-                    />
-                  ) : (
-                    ""
-                  )}
-                  {price.name === "Enterprise" ? (
-                    <img
-                      className={styles["pricing__item-image"]}
-                      src={enterpriseHoverSVG}
-                    />
-                  ) : (
-                    ""
-                  )}
-                  {price.name && (
-                    <h3 className={styles["pricing__item-title"]}>
-                      {price.name}
-                    </h3>
-                  )}
-                  <div className={styles["pricing__item-price"]}>
-                    <span className={styles["pricing__item-price-currency"]}>
-                      $
-                    </span>
-                    <h4 className={styles["pricing__item-price-title"]}>
-                      {price.price}
-                    </h4>
-                    <span className={styles["pricing__item-price-date"]}>
-                      /mo
-                    </span>
-                  </div>
-                  <p className={styles["pricing__item-subtitle"]}>
-                    {price.description}
-                  </p>
-                  <ul className={styles["pricing__item-features"]}>
-                    <li className={styles["pricing__item-features-item"]}>
-                      <Icon
-                        name="checkmark"
-                        className={styles["pricing__item-checkmark"]}
-                      />{" "}
-                      Up to {price.endusers} End Users
-                    </li>
-                    <li className={styles["pricing__item-features-item"]}>
-                      <Icon
-                        name="checkmark"
-                        className={styles["pricing__item-checkmark"]}
-                      />{" "}
-                      Feedback Translation
-                    </li>
-                    <li className={styles["pricing__item-features-item"]}>
-                      <Icon
-                        name="checkmark"
-                        className={styles["pricing__item-checkmark"]}
-                      />{" "}
-                      Emotion Analysis
-                    </li>
-                    <li className={styles["pricing__item-features-item"]}>
-                      <Icon
-                        name="checkmark"
-                        className={styles["pricing__item-checkmark"]}
-                      />{" "}
-                      Custom Domain
-                    </li>
-                  </ul>
-                  <div className={styles["pricing__item-button-wrapper"]}>
-                    <Button
-                      text="Try it Free"
-                      className={styles["pricing__item-button"]}
-                      to="/"
-                    />
-                  </div>
-                </li>
-              ))}
-          </ul>
-        </div>
-
-        <section className={styles["custom-pricing"]}>
-          <div className={styles["custom-pricing__heading"]}>
-            <h3 className={styles["custom-pricing__title"]}>
-              Expecting more than 1000 Active End Users?
-            </h3>
-            <p className={styles["custom-pricing__subtitle"]}>
-              We provide volume discounts for enterprise teams. Contact us to
-              learn more.
-            </p>
+          center={true}
+        >
+          <div className={styles["pricing__toggle"]}>
+            <span
+              className={`${styles["pricing__toggle-label"]} ${
+                styles["pricing__toggle-label--month"]
+              }`}
+            >
+              Monthly
+            </span>
+            <Switch toggle={this.togglePrice} />
+            <span
+              className={`${styles["pricing__toggle-label"]} ${
+                styles["pricing__toggle-label--year"]
+              }`}
+            >
+              Anually
+            </span>
           </div>
-          <Link to="#" className={styles["custom-pricing__button"]}>
-            Contact us
-          </Link>
-        </section>
 
-        <section className={styles["faq"]}>
-          <h3 className={styles["faq__title"]}>Frequently Asked Questions</h3>
-          <ul className={styles["faq__list"]}>
-            <li className={styles["faq__item"]}>
-              <h4 className={styles["faq__item-title"]}>
-                What are Active End Users?
-              </h4>
-              <p className={styles["faq__item-body"]}>
-                Active End Users are people that provide you with feedback. As
-                soon as they do, we flag them as active for the coming 3 months.
-                In case this person does not provide you with additional
-                feedback during these 3 months, the users gets flagged as
-                inactive. Inactive users will not be counted towards your
-                billable quota. This results in a bill that doesn't just
-                increase month over month.
+          {/* <Signup className={styles["pricing__signup"]} /> */}
+          <div className={styles["pricing__wrapper"]}>
+            <div className={styles["pricing__pattern"]} />
+            <ul className={styles["pricing__list"]}>
+              {pricingList &&
+                pricingList.map(price => (
+                  <li key={price._key} className={styles["pricing__item"]}>
+                    {price.name === "Hobby" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={hobbyHoverSVG}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Small Business" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={smallBusinessHoverSVG}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Company" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={companyHoverSVG}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Enterprise" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={enterpriseHoverSVG}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name && (
+                      <h3 className={styles["pricing__item-title"]}>
+                        {price.name}
+                      </h3>
+                    )}
+                    <div className={styles["pricing__item-price"]}>
+                      <span className={styles["pricing__item-price-currency"]}>
+                        $
+                      </span>
+                      <h4 className={styles["pricing__item-price-title"]}>
+                        {annualPrice
+                          ? Math.round((price.price * 12 * 0.9) / 12)
+                          : price.price}
+                      </h4>
+                      <span className={styles["pricing__item-price-date"]}>
+                        {annualPrice ? `/yr` : "/mo"}
+                      </span>
+                    </div>
+                    <p className={styles["pricing__item-subtitle"]}>
+                      {price.description}
+                    </p>
+                    <ul className={styles["pricing__item-features"]}>
+                      <li className={styles["pricing__item-features-item"]}>
+                        <Icon
+                          name="checkmark"
+                          className={styles["pricing__item-checkmark"]}
+                        />{" "}
+                        Up to {price.endusers} End Users
+                      </li>
+                      <li className={styles["pricing__item-features-item"]}>
+                        <Icon
+                          name="checkmark"
+                          className={styles["pricing__item-checkmark"]}
+                        />{" "}
+                        Feedback Translation
+                      </li>
+                      <li className={styles["pricing__item-features-item"]}>
+                        <Icon
+                          name="checkmark"
+                          className={styles["pricing__item-checkmark"]}
+                        />{" "}
+                        Emotion Analysis
+                      </li>
+                      <li className={styles["pricing__item-features-item"]}>
+                        <Icon
+                          name="checkmark"
+                          className={styles["pricing__item-checkmark"]}
+                        />{" "}
+                        Custom Domain
+                      </li>
+                    </ul>
+                    <div className={styles["pricing__item-button-wrapper"]}>
+                      <Button
+                        text="Try it Free"
+                        className={styles["pricing__item-button"]}
+                        to="/"
+                      />
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+
+          <section className={styles["custom-pricing"]}>
+            <div className={styles["custom-pricing__heading"]}>
+              <h3 className={styles["custom-pricing__title"]}>
+                Expecting more than 1000 Active End Users?
+              </h3>
+              <p className={styles["custom-pricing__subtitle"]}>
+                We provide volume discounts for enterprise teams. Contact us to
+                learn more.
               </p>
-            </li>
-            <li className={styles["faq__item"]}>
-              <h4 className={styles["faq__item-title"]}>
-                Can I always cancel my subscription?
-              </h4>
-              <p className={styles["faq__item-body"]}>
-                We offer a 60-day money back guarantee. If you cancel your
-                account afterwards we'll cancel your account at the end of your
-                billing term.
-              </p>
-            </li>
-            <li className={styles["faq__item"]}>
-              <h4 className={styles["faq__item-title"]}>
-                Do you offer Non-profit or Educational Discount?
-              </h4>
-              <p className={styles["faq__item-body"]}>
-                Yes! We'd love to support you in doing things that make the
-                world a better place. We offer a special discount to qualified
-                nonprofit organizations and educational institutions. Contact us
-                to learn more.
-              </p>
-            </li>
-          </ul>
-        </section>
-      </Page>
-    </Layout>
-  );
-};
+            </div>
+            <Link to="#" className={styles["custom-pricing__button"]}>
+              Contact us
+            </Link>
+          </section>
+
+          <section className={styles["faq"]}>
+            <h3 className={styles["faq__title"]}>Frequently Asked Questions</h3>
+            <ul className={styles["faq__list"]}>
+              <li className={styles["faq__item"]}>
+                <h4 className={styles["faq__item-title"]}>
+                  What are Active End Users?
+                </h4>
+                <p className={styles["faq__item-body"]}>
+                  Active End Users are people that provide you with feedback. As
+                  soon as they do, we flag them as active for the coming 3
+                  months. In case this person does not provide you with
+                  additional feedback during these 3 months, the users gets
+                  flagged as inactive. Inactive users will not be counted
+                  towards your billable quota. This results in a bill that
+                  doesn't just increase month over month.
+                </p>
+              </li>
+              <li className={styles["faq__item"]}>
+                <h4 className={styles["faq__item-title"]}>
+                  Can I always cancel my subscription?
+                </h4>
+                <p className={styles["faq__item-body"]}>
+                  We offer a 60-day money back guarantee. If you cancel your
+                  account afterwards we'll cancel your account at the end of
+                  your billing term.
+                </p>
+              </li>
+              <li className={styles["faq__item"]}>
+                <h4 className={styles["faq__item-title"]}>
+                  Do you offer Non-profit or Educational Discount?
+                </h4>
+                <p className={styles["faq__item-body"]}>
+                  Yes! We'd love to support you in doing things that make the
+                  world a better place. We offer a special discount to qualified
+                  nonprofit organizations and educational institutions. Contact
+                  us to learn more.
+                </p>
+              </li>
+            </ul>
+          </section>
+        </Page>
+      </Layout>
+    );
+  }
+}
 
 export default PricingTemplate;
