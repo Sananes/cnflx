94e7537cf17ce23582149aae5a584a571caa4f95
Aaron Sananes <aasananes@gmail.com>
2019-06-28 17:41:19 +0100

    Added pricing hooks

--- web/src/pages/PricingPage/PricingPage.js
+++ web/src/pages/PricingPage/PricingPage.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import React, { useState } from "react";
 import { Link, graphql } from "gatsby";
 import Icon from "../../components/Icon";
 import Svg from "../../components/Svg";
@@ -41,180 +41,163 @@ export const query = graphql`
   }
 `;
 
-class PricingTemplate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showPrice: false };
+const PricingTemplate = ({ data }) => {
+  const [showAnnual, setAnnual] = useState(false);
+
+  function togglePrice() {
+    setAnnual(!showAnnual);
   }
 
-  togglePrice = () => {
-    this.setState({
-      showPrice: !this.state.showPrice
-    });
-  };
+  const pricingList = data.sanityPricing._rawPackage;
+  const annualPrice = showAnnual;
 
-  setAnnual = e => {
-    e.preventDefault();
-    if (this.toggle === false) {
-      return this.togglePrice();
-    }
-  };
-
-  render() {
-    const { data } = this.props;
-    const pricingList = data.sanityPricing._rawPackage;
-    const annualPrice = this.state.showPrice;
-
-    return (
-      <Layout title={`Pricing`}>
-        <Page
-          title="Pricing that suits your needs"
-          subtitle="
+  return (
+    <Layout title={`Pricing`}>
+      <Page
+        title="Pricing that suits your needs"
+        subtitle="
         Conflux is perfect for individuals and teams of all sizes. Start for free."
-          center={true}
-        >
-          <div className={styles["pricing__toggle"]}>
-            <div className={styles["pricing__toggle-discount"]}>
-              Save up to 10% annualy
-              <Svg
-                name="pricingArrow"
-                className={styles["pricing__toggle-arrow"]}
-              />
-            </div>
-            <span
-              className={`${styles["pricing__toggle-label"]} ${
-                styles["pricing__toggle-label--month"]
-              }`}
-            >
-              <span className={styles["pricing__toggle-label-link"]}>
-                Monthly
-              </span>
-            </span>
-            <Switch toggle={this.togglePrice} />
-            <span
-              className={`${styles["pricing__toggle-label"]} ${
-                styles["pricing__toggle-label--year"]
-              }`}
-            >
-              <span className={styles["pricing__toggle-label-link"]}>
-                Anually
-              </span>
-            </span>
+        center={true}
+      >
+        <div className={styles["pricing__toggle"]}>
+          <div className={styles["pricing__toggle-discount"]}>
+            Save up to 10% annualy
+            <Svg
+              name="pricingArrow"
+              className={styles["pricing__toggle-arrow"]}
+            />
           </div>
+          <span
+            className={`${styles["pricing__toggle-label"]} ${
+              styles["pricing__toggle-label--month"]
+            }`}
+          >
+            <span className={styles["pricing__toggle-label-link"]}>
+              Monthly
+            </span>
+          </span>
+          <Switch toggle={togglePrice} />
+          <span
+            className={`${styles["pricing__toggle-label"]} ${
+              styles["pricing__toggle-label--year"]
+            }`}
+          >
+            <span className={styles["pricing__toggle-label-link"]}>
+              Anually
+            </span>
+          </span>
+        </div>
 
-          {/* <Signup className={styles["pricing__signup"]} /> */}
-          <div className={styles["pricing__wrapper"]}>
-            <div className={styles["pricing__pattern"]} />
-            <ul className={styles["pricing__list"]}>
-              {pricingList &&
-                pricingList.map(price => (
-                  <li key={price._key} className={styles["pricing__item"]}>
-                    <div className={styles["pricing__item-top-wrapper"]}>
-                      {price.name === "Hobby" ? (
-                        <img
-                          className={styles["pricing__item-image"]}
-                          alt={price.name}
-                          src={hobbyHoverSVG}
-                        />
-                      ) : (
-                        ""
-                      )}
-                      {price.name === "Small Business" ? (
-                        <img
-                          className={styles["pricing__item-image"]}
-                          src={smallBusinessHoverSVG}
-                          alt={price.name}
-                        />
-                      ) : (
-                        ""
-                      )}
-                      {price.name === "Company" ? (
-                        <img
-                          className={styles["pricing__item-image"]}
-                          src={companyHoverSVG}
-                          alt={price.name}
-                        />
-                      ) : (
-                        ""
-                      )}
-                      {price.name === "Enterprise" ? (
-                        <img
-                          className={styles["pricing__item-image"]}
-                          src={enterpriseHoverSVG}
-                          alt={price.name}
-                        />
-                      ) : (
-                        ""
-                      )}
-                      {price.name && (
-                        <h3 className={styles["pricing__item-title"]}>
-                          {price.name}
-                        </h3>
-                      )}
-                      <div className={styles["pricing__item-price"]}>
-                        <span
-                          className={styles["pricing__item-price-currency"]}
-                        >
-                          €
-                        </span>
-                        <h4 className={styles["pricing__item-price-title"]}>
-                          {annualPrice
-                            ? Math.round((price.price * 12 * 0.9) / 12)
-                            : price.price}
-                        </h4>
-                        <span className={styles["pricing__item-price-date"]}>
-                          per <br /> month
-                        </span>
-                      </div>
-                      <p className={styles["pricing__item-subtitle"]}>
-                        {price.description}
-                      </p>
+        {/* <Signup className={styles["pricing__signup"]} /> */}
+        <div className={styles["pricing__wrapper"]}>
+          <div className={styles["pricing__pattern"]} />
+          <ul className={styles["pricing__list"]}>
+            {pricingList &&
+              pricingList.map(price => (
+                <li key={price._key} className={styles["pricing__item"]}>
+                  <div className={styles["pricing__item-top-wrapper"]}>
+                    {price.name === "Hobby" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        alt={price.name}
+                        src={hobbyHoverSVG}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Small Business" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={smallBusinessHoverSVG}
+                        alt={price.name}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Company" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={companyHoverSVG}
+                        alt={price.name}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name === "Enterprise" ? (
+                      <img
+                        className={styles["pricing__item-image"]}
+                        src={enterpriseHoverSVG}
+                        alt={price.name}
+                      />
+                    ) : (
+                      ""
+                    )}
+                    {price.name && (
+                      <h3 className={styles["pricing__item-title"]}>
+                        {price.name}
+                      </h3>
+                    )}
+                    <div className={styles["pricing__item-price"]}>
+                      <span className={styles["pricing__item-price-currency"]}>
+                        €
+                      </span>
+                      <h4 className={styles["pricing__item-price-title"]}>
+                        {annualPrice
+                          ? Math.round((price.price * 12 * 0.9) / 12)
+                          : price.price}
+                      </h4>
+                      <span className={styles["pricing__item-price-date"]}>
+                        per <br /> month
+                      </span>
                     </div>
-                    <div className={styles["pricing__item-wrapper"]}>
-                      <ul className={styles["pricing__item-features"]}>
-                        <li className={styles["pricing__item-features-item"]}>
-                          <Icon
-                            name="checkmark"
-                            className={styles["pricing__item-checkmark"]}
-                          />{" "}
-                          Up to {price.endusers} End Users
-                        </li>
-                      </ul>
-                      <div className={styles["pricing__item-button-wrapper"]}>
-                        <Button
-                          text="Try it Free"
-                          className={styles["pricing__item-button"]}
-                          to="/"
-                        />
-                      </div>
+                    <p className={styles["pricing__item-subtitle"]}>
+                      {price.description}
+                    </p>
+                  </div>
+                  <div className={styles["pricing__item-wrapper"]}>
+                    <ul className={styles["pricing__item-features"]}>
+                      <li className={styles["pricing__item-features-item"]}>
+                        <Icon
+                          name="checkmark"
+                          className={styles["pricing__item-checkmark"]}
+                        />{" "}
+                        Up to {price.endusers} End Users
+                      </li>
+                    </ul>
+                    <div className={styles["pricing__item-button-wrapper"]}>
+                      <Button
+                        text="Try it Free"
+                        className={styles["pricing__item-button"]}
+                        to="/"
+                      />
                     </div>
-                  </li>
-                ))}
-            </ul>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
+
+        <section className={styles["custom-pricing"]}>
+          <div className={styles["custom-pricing__heading"]}>
+            <h3 className={styles["custom-pricing__title"]}>
+              Expecting more than 1000 Active End Users?
+            </h3>
+            <p className={styles["custom-pricing__subtitle"]}>
+              We provide volume discounts for enterprise teams. Contact us to
+              learn more.
+            </p>
           </div>
+          <Link to="#" className={styles["custom-pricing__button"]}>
+            Contact us
+          </Link>
+        </section>
 
-          <section className={styles["custom-pricing"]}>
-            <div className={styles["custom-pricing__heading"]}>
-              <h3 className={styles["custom-pricing__title"]}>
-                Expecting more than 1000 Active End Users?
-              </h3>
-              <p className={styles["custom-pricing__subtitle"]}>
-                We provide volume discounts for enterprise teams. Contact us to
-                learn more.
-              </p>
-            </div>
-            <Link to="#" className={styles["custom-pricing__button"]}>
-              Contact us
-            </Link>
-          </section>
+        <Partners className={styles["pricing-partners"]} />
 
-          <Partners className={styles["pricing-partners"]} />
-
-          <Faqs className={styles["pricing-faq"]} />
-        </Page>
-      </Layout>
-    );
-  }
-}
+        <Faqs className={styles["pricing-faq"]} />
+      </Page>
+    </Layout>
+  );
+};
 
 export default PricingTemplate;
