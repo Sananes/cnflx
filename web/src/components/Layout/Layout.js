4c08103c484da05db8dcb93ca57a62dafb0ccd9e
Aaron Sananes <aasananes@gmail.com>
2022-10-18 09:40:14 +0100

    Update Layout.js

--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -34,7 +34,6 @@ const Layout = ({ children, title, description, className }: Props) => {
           rel="stylesheet"
         />
       </Helmet>
-      <>
       <div className={`${styles["inner"]} layout-inner`}>
         <Header />
         {children}
@@ -51,12 +50,11 @@ const Layout = ({ children, title, description, className }: Props) => {
           We use cookies to ensure you get the best experience.{" "}
           <Link to="/cookies-policy">Learn more</Link>
         </CookieConsent>
-      </div>
-    </div>
-    <script>
+            <script>
         (function(d,h,w){var gist=w.gist=w.gist||[];gist.methods=['trackPageView','identify','track','setAppId'];gist.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);gist.push(e);return gist;}};for(var i=0;i<gist.methods.length;i++){var c=gist.methods[i];gist[c]=gist.factory(c)}s=d.createElement('script'),s.src="https://widget.getgist.com",s.async=!0,e=d.getElementsByTagName(h)[0],e.appendChild(s),s.addEventListener('load',function(e){},!1),gist.setAppId("dmywdm46"),gist.trackPageView()})(document,'head',window);
     </script>
-    </>
+      </div>
+    </div>
   );
 };
 
