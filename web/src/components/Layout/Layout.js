8d0500b5e199c691ff16fe78c4c6090bbdc96aed
Aaron Sananes <aasananes@gmail.com>
2022-10-18 09:26:05 +0100

    Update Layout.js

--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -34,6 +34,7 @@ const Layout = ({ children, title, description, className }: Props) => {
           rel="stylesheet"
         />
       </Helmet>
+      <>
       <div className={`${styles["inner"]} layout-inner`}>
         <Header />
         {children}
@@ -55,6 +56,7 @@ const Layout = ({ children, title, description, className }: Props) => {
     <script>
         (function(d,h,w){var gist=w.gist=w.gist||[];gist.methods=['trackPageView','identify','track','setAppId'];gist.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);gist.push(e);return gist;}};for(var i=0;i<gist.methods.length;i++){var c=gist.methods[i];gist[c]=gist.factory(c)}s=d.createElement('script'),s.src="https://widget.getgist.com",s.async=!0,e=d.getElementsByTagName(h)[0],e.appendChild(s),s.addEventListener('load',function(e){},!1),gist.setAppId("dmywdm46"),gist.trackPageView()})(document,'head',window);
     </script>
+    </>
   );
 };
 
